132efe10d1c6359ed06a062e6a567a94ed67434a
pgm-stefdebo3 <stef.deboeck@student.arteveldehs.be>
2021-12-06 12:51:11 +0100

    initial commit

---
+++ data/atelier.js
@@ -0,0 +1,42 @@
+const ATELIER = [{
+    title: "Painting for our health workers",
+    subtitle: "A Wildflower Garden for the local hospital workers",
+    description: "During this past week, Arne Quinze has been working on a painting which he will donate to the...",
+    images: ["arne-quinze-wildflowers-palfijn.gif"],
+    link: "learn more",
+},
+{
+    title: "Cocooning in my painting atelier",
+    subtitle: "Wildflower Fields paintings",
+    description: "We all need that quiet retreat from chaos in the world and at home: a sanctuary, where creative...",
+    images: ["04ce845e2edb124405b941bb3f8db355.jpg"],
+    link: "learn more",
+},
+{
+    title: "Storm Ciara seen from a Rock Strangers perspective",
+    subtitle: "People braving storm Ciara",
+    description: "Ostend, February 10th 2020. Many people are attracted by the spectacle of the sea in combination...",
+    images: ["montage.gif"],
+    link: "learn more",
+},
+{
+    title: "My Secret Garden, Valencia — Extended until 1st of March",
+    subtitle: "Figures to be proud of",
+    description: "Since February 2019, the open-air exhibition of Arne Quinze already attracted 3 million visitors,...",
+    images: ["f1d8f308030ba408c13e4b39d22b62c1.jpg"],
+    link: "learn more",
+},
+{
+    title: "Planting the Apple Tree Saver",
+    subtitle: "Private garden sculpture",
+    description: "A dark Winter day, if it wasn't for this sculpture to get installed in the garden of an art...",
+    images: ["34f35a10c79a6d37b0e65bae28812024.jpg"],
+    link: "learn more",
+},
+{
+    title: "Painting new Chroma Lupines",
+    subtitle: "Indoor Chroma wall flowers",
+    description: "Arne Quinze at work, painting new medium-sized sculptures that would definitely catch the eye in...",
+    images: ["2a2adc91a3c7546bccfa838a3e5b7b22.jpg"],
+    link: "learn more",
+},]
---
+++ js/main.js
@@ -0,0 +1,131 @@
+(() => {
+
+    const app = {
+        initialize() {
+            this.cacheElements();
+            this.buildUI();
+            this.addEventListeners();
+        },
+        buildUI() {
+            try {
+            this.$highlightedArt.innerHTML = this.generateHTMLforHighlightedArt();
+            }
+            catch {
+                this.stoperror()
+            }
+            try {
+            this.$highlightedAtelier.innerHTML = this.generateHTMLforHighlightedAtelier();
+            }
+            catch {
+                this.stoperror()
+            }
+            try {
+            this.$Atelier.innerHTML = this.generateHTMLforAtelier();
+            }
+            catch {
+                this.stoperror()
+            }
+            try {
+            this.$PressReleases.innerHTML = this.generateHTMLforPressReleases();
+            }
+            catch {
+                this.stoperror()
+            }
+            try {
+            this.$InPress.innerHTML = this.generateHTMLforInPress();
+            }
+            catch {
+                this.stoperror()
+            }
+        },
+        generateHTMLforHighlightedArt() {
+            let tempArr = ARTS.filter(artItem => artItem.highlight === true);
+            return tempArr.map((element) => {
+                return `
+            <div class="highlighted_art_article">
+            <img src="static/img/art/afbeeldingen/${element.images[0]}" alt="hihglighted art image" loading="lazy">
+            <p class="gray_sub_title">${element.tags.join('/')} — ${element.location}</p>
+            <h2>${element.title}</h2>
+            <p>${element.description}</p>
+            <a href="art-and-exhibitions/detail/index.html">Learn more</a>
+            </div>
+            `
+            });
+        },
+        generateHTMLforHighlightedAtelier() {
+            let tempArr = ATELIER.slice(0, 3);
+            return tempArr.map((element) => {
+                return `
+            <div class="highlighted_atelier_article">
+            <img src="static/img/atelier/${element.images[0]}" alt="highlighted atelier image" loading="lazy">
+            <p class="gray_sub_title">${element.subtitle}</p>
+            <h2>${element.title}</h2>
+            <p>${element.description}</p>
+            <a href="atelier-studio/detail/index.html">${element.link}</a>
+            </div>
+            `
+            });
+        },
+        generateHTMLforAtelier() {
+            let tempArr = ATELIER;
+            return tempArr.map((element) => {
+                return `
+            <div class="atelier_article">
+            <img src="static/img/atelier/${element.images[0]}" alt="atelier image" loading="lazy">
+            <p class="gray_sub_title">${element.subtitle}</p>
+            <h2>${element.title}</h2>
+            <p>${element.description}</p>
+            <a href="atelier-studio/detail/index.html">${element.link}</a>
+            </div>
+            `
+            });
+        },
+        generateHTMLforPressReleases() {
+            let tempArr = PRESS.filter(pressItem => pressItem.type === "press release");
+            return tempArr.map((element) => {
+                return `
+            <div class="press_release_article">
+            <img src="static/img/press/${element.images}" alt="press image" loading="lazy">
+            <p class="gray_sub_title">${element.subtitle}</p>
+            <h2>${element.title}</h2>
+            <p>${element.description}</p>
+            <a href="atelier-studio/detail/index.html">${element.link}</a>
+            </div>
+            `
+            });
+        },
+        generateHTMLforInPress() {
+            let tempArr = PRESS.filter(pressItem => pressItem.type === "in the press");
+            return tempArr.map((element) => {
+                return `
+            <div class="in_the_press_article">
+            <img src="static/img/press/${element.images}" alt="press image" loading="lazy">
+            <p class="gray_sub_title">${element.subtitle}</p>
+            <h2>${element.title}</h2>
+            <p>${element.description}</p>
+            <a href="atelier-studio/detail/index.html">${element.link}</a>
+            </div>
+            `
+            });
+        },
+
+        addEventListeners() {
+
+        },
+        cacheElements() {
+            this.$highlightedArt = document.querySelector(".index_highlighted_art_dynamic")
+            this.$highlightedAtelier = document.querySelector(".index_highlighted_atelier_dynamic")
+            this.$Atelier = document.querySelector(".Atelier_dynamic")
+            this.$PressReleases = document.querySelector(".press_release_articles")
+            this.$InPress = document.querySelector(".in_press_articles")
+        },
+        stoperror() {
+            return true;
+        }
+    };
+    app.initialize();
+
+
+
+})();
+
